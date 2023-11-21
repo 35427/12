02f54354ebe8ccb7d35837f240348dfab09362bb
35427 <141239826+35427@users.noreply.github.com>
2023-11-21 16:37:25 +0900

    11/21

---
+++ board.h
@@ -0,0 +1,11 @@
+#define BOARDSTATUS_OK		1 //값이 1이면 OK 
+#define BOARDSTATUS_NOK		0//값이 0이면 Not ok, =파손
+
+int board_initboard(void);
+int board_printBoardStatus(void);
+
+int board_getBoardCoin(int pos);
+int board_getSharkPosition(void);
+
+int board_stepShark(void);
+int board_getBoardStatus(int pos);
